7108f138cee1feeea32de14c08ee2c2e0062b6e3
Sam Susikar <samsusikar@Sams-MacBook-Pro.local>
2024-04-28 18:35:58 +0530

    header fixed, animation has to be done

--- main.js
+++ main.js
@@ -2,16 +2,30 @@ document.addEventListener("DOMContentLoaded", function() {
     var pentagon = document.querySelector(".icon.pentagon path");
 
     pentagon.style.animation = "animate-logo 2s linear forwards";
-
 });
 
 window.addEventListener("scroll", function() {
     var container = document.querySelector(".container");
+    var scrolldown = document.querySelector(".scrollDown");
 
-    if (window.scrollY > 100) { // Adjust the value as needed
+    if (window.scrollY > 1) {
         container.classList.add("move-top-left");
+        scrolldown.classList.add("fade-out"); 
     } else {
         container.classList.remove("move-top-left");
+        scrolldown.classList.remove("fade-out");
     }
 });
 
+// to hide the name thing
+
+window.addEventListener("scroll", function() {
+    var page1 = document.getElementById("page1");
+    var titleBar = document.getElementById("titleBar");
+
+    if (window.scrollY > page1.clientHeight) {
+        titleBar.style.display = "block"; // Show the title bar
+    } else {
+        titleBar.style.display = "none"; // Hide the title bar
+    }
+});
